32867eaad01dbf0875e0378ccea5b8f498eba967
bertdawg76 <bertcoleman76@gmail.com>
2016-07-22 19:15:52 -0600

    first commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,32 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Nav = () => {
+
+  return (
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">React Time App</li>
+          <li>
+            <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Timer</IndexLink>
+          </li>
+          <li>
+            <Link to="/Countdown" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Countdown</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="top-bar-right">
+        <ul className="menu">
+          <li className="menu-text">
+            Created by <a href="https://www.github.com/bertdawg76" target="_blank">Bert Coleman</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+};
+
+
+
+module.exports = Nav;
